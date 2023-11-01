0a03d8bf9488ff6a809115a055040846a3e2985b
Agilar011 <88954314+Agilar011@users.noreply.github.com>
2023-11-01 14:02:49 +0700

    update splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:uts_mobile/layouts/LoginScreen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,17 +31,17 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Your custom icon
-            Icon(
-              Icons.ac_unit, // Replace with your custom icon
-              size: 100.0,
-              color: Colors.blue, // Change icon color as needed
+            // Your custom image from 'assets' folder
+            Image(
+              image: AssetImage('assets/images/images.jpeg'),
+              width: 100.0,
+              height: 100.0,
             ),
             SizedBox(height: 20),
             // Progress indicator (using flutter_spinkit)
-            SpinKitFadingCube(
+            SpinKitRing(
               color: Colors.blue, // Change the loading indicator color
-              size: 50.0, // Adjust the size as needed
+              size: 20.0, // Adjust the size as needed
             ),
           ],
         ),
